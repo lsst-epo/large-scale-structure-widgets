cf1195455ded4e8265a8ab524f08145c88e94243
parkerkaufmann <parkerkaufmann@email.arizona.edu>
2019-10-11 10:05:25 -0700

    [C] Changed function styles & return statements
    EPO-2255
    EPO-2187
    EPO-2185

--- src/components/site/Practice.jsx
+++ src/components/site/Practice.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactEcharts from 'echarts-for-react';
+import isEqual from 'lodash/isEqual';
 import API from './API';
 
 class Practice extends React.PureComponent {
@@ -12,7 +13,7 @@ class Practice extends React.PureComponent {
       clickedPt: {},
     };
     this.onChartClick = this.onChartClick.bind(this);
-    this.getOption = this.getOption.bind(this);
+    // this.getOption = this.getOption.bind(this);
   }
 
   componentDidMount() {
@@ -25,12 +26,12 @@ class Practice extends React.PureComponent {
     });
   }
 
-  onChartClick(params) {
+  onChartClick = params => {
     this.setState(prevState => ({
       ...prevState,
       clickedPt: params.value,
     }));
-  }
+  };
 
   getEvent() {
     const onEvents = {
@@ -39,91 +40,92 @@ class Practice extends React.PureComponent {
     return onEvents;
   }
 
+  getColor = params => {
+    const { clickedPt } = this.state;
+    const { value } = params;
+    const defaultColor = '#e97a7a';
+    if (value == null) {
+      return '';
+    }
+    if (isEqual(value, clickedPt)) {
+      return '#b7b7ff';
+    }
+    return defaultColor;
+  };
+
+  getTooltipFormat = params => {
+    const dat = params.value;
+    return dat.RA.toFixed(2) + ', ' + dat.Dec.toFixed(2);
+  };
+
   getOption() {
-    const { data, clickedPt } = this.state;
-    let obj = {};
-    if (Object.keys(data).length !== 0) {
-      obj = {
-        animation: false,
-        title: {
-          text: 'Exploring Observable Universe',
-        },
-        legend: {
-          orient: 'vertical',
-          right: 5,
-        },
-        dataset: {
-          source: data.galaxies,
-          dimensions: ['RA', 'Dec'],
-        },
-        tooltip: {
-          formatter: params => {
-            const dat = params.value;
-            return dat.RA.toFixed(2) + ', ' + dat.Dec.toFixed(2);
-          },
-        },
-        xAxis: {
-          name: 'RA',
-          nameLocation: 'middle',
-          nameGap: 25,
-        },
-        yAxis: {
-          name: 'Dec',
-          nameLocation: 'middle',
-          nameGap: 25,
-        },
-        series: [
-          {
-            name: 'Data Point',
-            symbolSize: 8,
-            type: 'scatter',
-            itemStyle: {
-              normal: {
-                color: params => {
-                  const { value } = params;
-                  const defaultColor = '#e97a7a';
-                  if (value == null) {
-                    return '';
-                  } else if (
-                    clickedPt.RA === value.RA &&
-                    clickedPt.Dec === value.Dec
-                  ) {
-                    return '#b7b7ff';
-                  } else {
-                    return defaultColor;
-                  }
-                },
-              },
+    const { data } = this.state;
+    if (Object.keys(data).length === 0) {
+      return {};
+    }
+    return {
+      animation: false,
+      title: {
+        text: 'Exploring Observable Universe',
+      },
+      legend: {
+        orient: 'vertical',
+        right: 5,
+      },
+      dataset: {
+        source: data.galaxies,
+        dimensions: ['RA', 'Dec'],
+      },
+      tooltip: {
+        formatter: this.getTooltipFormat,
+      },
+      xAxis: {
+        name: 'RA',
+        nameLocation: 'middle',
+        nameGap: 25,
+      },
+      yAxis: {
+        name: 'Dec',
+        nameLocation: 'middle',
+        nameGap: 25,
+      },
+      series: [
+        {
+          name: 'Data Point',
+          symbolSize: 8,
+          type: 'scatter',
+          itemStyle: {
+            normal: {
+              color: this.getColor,
             },
           },
-        ],
-        dataZoom: [
-          {
-            type: 'slider',
-            show: false,
-            xAxisIndex: [0],
-            minSpan: 5,
-          },
-          {
-            type: 'slider',
-            show: false,
-            yAxisIndex: [0],
-            minSpan: 5,
-          },
-          {
-            type: 'inside',
-            xAxisIndex: [0],
-            minSpan: 5,
-          },
-          {
-            type: 'inside',
-            yAxisIndex: [0],
-            minSpan: 5,
-          },
-        ],
-      };
-    }
-    return obj;
+        },
+      ],
+      dataZoom: [
+        {
+          type: 'slider',
+          show: false,
+          xAxisIndex: [0],
+          minSpan: 60,
+        },
+        {
+          type: 'slider',
+          show: false,
+          yAxisIndex: [0],
+          minSpan: 60,
+        },
+        {
+          type: 'inside',
+          xAxisIndex: [0],
+          minSpan: 60,
+        },
+        {
+          type: 'inside',
+          yAxisIndex: [0],
+          minSpan: 60,
+        },
+      ],
+    };
   }
 
   render() {
